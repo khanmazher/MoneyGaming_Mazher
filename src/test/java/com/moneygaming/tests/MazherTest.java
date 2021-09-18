cb21769b04452529bc662921ff1257e09aac8fc9
khanmazher <khanmazher@gmail.com>
2021-09-18 19:36:38 +0300

    Mazher branch commit

---
+++ src/test/java/com/moneygaming/tests/MazherTest.java
@@ -0,0 +1,11 @@
+package com.moneygaming.tests;
+
+import org.testng.annotations.Test;
+
+public class MazherTest {
+
+    @Test
+    public void testMazher(){
+        System.out.println("my beautiful test");
+    }
+}
